a9244dc9e934b6e60ca77d1b770737b48c4d519a
Natalia Bashirova <tac-2014@yandex.ru>
2024-11-27 22:42:05 +0100

    Исправляет ошибки аудита

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -8,9 +8,7 @@ type WelcomeScreen = {
 function WelcomeScreen({ amountPlaces }: WelcomeScreen): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <Header>
-      </Header>
-
+      <Header></Header>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
