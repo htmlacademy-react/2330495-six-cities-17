9d0bf8e29181c159f69d305d31ab56d946ab7099
Natalia Bashirova <tac-2014@yandex.ru>
2024-12-07 18:30:20 +0100

    Выносит всю разметку по по предложениям на главной странице в отдельный компонент

--- src/components/cities-places-list/cities-places-list.tsx
+++ src/components/cities-places-list/cities-places-list.tsx
@@ -1,20 +1,52 @@
-
-
 import Card from '../card/card';
 import { Offers } from '../../types/offers';
 
 type CitiesPlacesLisProps = {
+  amountPlaces: number;
   offers: Offers;
   cardClassName: string;
 };
 
-function CitiesPlacesList ({offers,cardClassName}:CitiesPlacesLisProps): JSX.Element {
+function CitiesPlacesList({
+  amountPlaces,
+  offers,
+  cardClassName,
+}: CitiesPlacesLisProps): JSX.Element {
   return (
-    <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer) => (
-        <Card key={offer.id} offer={offer} cardClassName={cardClassName}/>
-      ))}
-    </div>
+    <section className="cities__places places">
+      <h2 className="visually-hidden">Places</h2>
+      <b className="places__found">
+        {amountPlaces} places to stay in Amsterdam
+      </b>
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+        <span className="places__sorting-type" tabIndex={0}>
+          Popular
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+        <ul className="places__options places__options--custom places__options--opened">
+          <li className="places__option places__option--active" tabIndex={0}>
+            Popular
+          </li>
+          <li className="places__option" tabIndex={0}>
+            Price: low to high
+          </li>
+          <li className="places__option" tabIndex={0}>
+            Price: high to low
+          </li>
+          <li className="places__option" tabIndex={0}>
+            Top rated first
+          </li>
+        </ul>
+      </form>
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer) => (
+          <Card key={offer.id} offer={offer} cardClassName={cardClassName} />
+        ))}
+      </div>
+    </section>
   );
 }
 
