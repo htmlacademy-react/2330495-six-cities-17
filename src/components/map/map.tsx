a52183a19c4556d4ff5a35c7047159b72f8d0ff9
Natalia Bashirova <tac-2014@yandex.ru>
2024-12-10 17:56:11 +0100

    Добавляет карту

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react';
-import leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+// import React, { useRef } from 'react';
+// import leaflet from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
 
 function Map() {
-  const mapRef = useRef(null);
+  // const mapRef = useRef(null);
   return <div style={{ height: '500px' }}></div>;
 }
 
