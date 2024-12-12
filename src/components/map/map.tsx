9ef980fe3f8cfe5dfb527676161cefeed712b7a1
Natalia Bashirova <tac-2014@yandex.ru>
2024-12-12 22:30:54 +0100

    Добавляет карту

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect } from 'react';
 import {Icon, Marker, layerGroup} from 'leaflet';
 import useMap from '../../hooks/use-map';
-// import leaflet from 'leaflet';
 import { URL_MARKER_CURRENT,URL_MARKER_DEFAULT } from '../../const';
 import 'leaflet/dist/leaflet.css';
 import { City, Locations } from '../../types/offer';
