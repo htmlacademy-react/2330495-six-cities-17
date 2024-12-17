6a60aeda95e5f2c8ab43729662db8d809a7a280a
NataBash <2864tasha@gmail.com>
2024-12-17 16:24:39 +0100

    Подключит redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import offerListTotal from './mocks/offers';
 import fullInfoOffer from './mocks/offer';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,12 +12,14 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      // amountPlaces={amountPlaces}
-      offers={offerListTotal}
-      fullOffer ={fullInfoOffer}
-      city={fullInfoOffer.city}
-      points={offerListTotal.map((offer) => offer.location)}
-    />
+    <Provider store = {store}>
+      <App
+        // amountPlaces={amountPlaces}
+        offers={offerListTotal}
+        fullOffer={fullInfoOffer}
+        city={fullInfoOffer.city}
+        points={offerListTotal.map((offer) => offer.location)}
+      />
+    </Provider>
   </React.StrictMode>
 );
