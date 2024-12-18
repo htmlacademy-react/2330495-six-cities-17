30c703713ec24149cdf52f6bc80e4517ea15c3f3
NataBash <2864tasha@gmail.com>
2024-12-18 14:09:41 +0100

    Меняет внешний вид пойнтов

--- src/const.ts
+++ src/const.ts
@@ -21,10 +21,10 @@ export enum AuthorizationStatus {
 }
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+ './public/img/pin.svg';
 
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+ './public/img/pin-active.svg';
 
 export enum ActionType {
   ChangeCity = 'CHANGE_CITY',
