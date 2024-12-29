1629579296f07c5d83d4081f4551c00bc480cea4
NataBash <2864tasha@gmail.com>
2024-12-29 14:52:10 +0100

    Добавит признак наличия авторизации в глобальное состояние приложения

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,21 +1,9 @@
-import {createAction} from '@reduxjs/toolkit';
-// import { ActionType } from '../const';
-import { Town } from '../const';
+import { createAction } from '@reduxjs/toolkit';
+import { Town, AuthorizationStatus } from '../const';
 import { Offer } from '../types/offers';
-
-
-// type ChangeCitiAction ={
-//   type: ActionType.ChangeCity;
-//   payload: Town;
-// }
-
-// type LoadOffersAction = {
-//   type: ActionType.LoadOffers;
-//   payload: Offer[];
-// }
-// type Action = ChangeCitiAction| LoadOffersAction;
-
 
 export const changeCity = createAction<Town>('main/changeCity');
 export const loadOffers = createAction<Offer[]>('main/loadOffers');
-
+export const requireAuthorization = createAction<AuthorizationStatus>(
+  'user/requireAuthorization'
+);
