c6b9d5a177ec9bb843553d4b34ed8c89fa7c7f68
NataBash <2864tasha@gmail.com>
2024-12-29 14:12:59 +0100

    Добавляет поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, InternalAxiosRequestConfig} from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -9,5 +10,18 @@ export const createAPI = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
+
   return api;
 };
+
