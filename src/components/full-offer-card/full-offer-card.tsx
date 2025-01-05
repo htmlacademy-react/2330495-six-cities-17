bf856baa091183fe096fd98d38a4c587a1783a4e
NataBash <2864tasha@gmail.com>
2025-01-05 17:00:53 +0100

    Загружает фото для оффера

--- src/components/full-offer-card/full-offer-card.tsx
+++ src/components/full-offer-card/full-offer-card.tsx
@@ -18,6 +18,7 @@ function FullOfferCard({
     type,
     bedrooms,
     maxAdults,
+    images,
     // rating,
     // id,
     // isPremium,
@@ -26,54 +27,21 @@ function FullOfferCard({
 
   // const ratingPercentage = `${Math.round(rating) * 20}%`;
   if (!currentOffer || !currentOffer.goods) {
-    return null; // Или можно отобразить сообщение о загрузке
+    return null;
   }
   return (
     <section className="offer">
       <div className="offer__gallery-container container">
         <div className="offer__gallery">
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/room.jpg"
-              alt="Photo studio"
-            />
-          </div>
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/apartment-01.jpg"
-              alt="Photo studio"
-            />
-          </div>
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/apartment-02.jpg"
-              alt="Photo studio"
-            />
-          </div>
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/apartment-03.jpg"
-              alt="Photo studio"
-            />
-          </div>
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/studio-01.jpg"
-              alt="Photo studio"
-            />
-          </div>
-          <div className="offer__image-wrapper">
-            <img
-              className="offer__image"
-              src="img/apartment-01.jpg"
-              alt="Photo studio"
-            />
-          </div>
+          {images?.map((image, index) => (
+            <div className="offer__image-wrapper" key={image}>
+              <img
+                className="offer__image"
+                src={image}
+                alt={`Photo ${index + 1}`}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className="offer__container container">
