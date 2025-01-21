18947335d75888c7fcef552972d7da267b7a6ea5
NataBash <2864tasha@gmail.com>
2025-01-21 22:35:57 +0100

    Перенаправляет зарегестрированного пользователя на страницу в избранное

--- src/components/header-nav/header-nav.tsx
+++ src/components/header-nav/header-nav.tsx
@@ -4,6 +4,7 @@ import { AuthorizationStatus } from '../../const';
 import { logoutAction } from '../../store/api-actions';
 import { RootState } from '../../types/state';
 import { useAuthorizationStatus } from '../../hooks/use-authorization-status';
+import { AppRoute } from '../../const';
 
 function HeaderNav(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -24,7 +25,7 @@ function HeaderNav(): JSX.Element {
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-item user">
-            <Link className="header__nav-link header__nav-link--profile" to="#">
+            <Link className="header__nav-link header__nav-link--profile" to="/favorites">
               <div className="header__avatar-wrapper user__avatar-wrapper"></div>
               <span className="header__user-name user__name">{user.email}</span>
               <span className="header__favorite-count">3</span>
