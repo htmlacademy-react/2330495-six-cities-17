fa26f5511d6c6b2744842203837bc68fef43397a
NataBash <2864tasha@gmail.com>
2025-01-21 22:28:18 +0100

    Исправляет ошибку с несохраннеными неавторизованными данными

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -47,7 +47,9 @@ export const checkAuthAction = createAsyncThunk<
 >('user/checkAuth', async (_arg, { dispatch, extra: api }) => {
   try {
     await api.get(APIRoute.Login);
+    const { data } = await api.get<UserData>(APIRoute.Login);
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(setUser(data));
   } catch {
     dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
   }
